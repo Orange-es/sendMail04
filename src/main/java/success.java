f00ad469e12f5d64344f1f8aec9bbce61c143163
Orange-es <54025960+Orange-es@users.noreply.github.com>
2022-03-30 21:19:58 +0800

    Update success.java

--- src/main/java/success.java
+++ src/main/java/success.java
@@ -6,7 +6,7 @@
 public class success {
     public static void main(String[] args) throws Exception {
         String mailAccount = "786788651@qq.com";
-        String mailPwd = "";//填写邮箱验证密码
+        String mailPwd = args[0].toString();//填写邮箱验证密码
         System.out.println( "Hello World!" );
         SendMail sendMail = new SendMail();
         String s = sendMail.send(mailAccount, mailPwd, mailAccount, "测试！", "发邮件！");
